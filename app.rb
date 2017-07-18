11e9a73e34af8e0817f03ecfb76071151c000520
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-07-18 16:01:28 -0400

    web app for name and age

--- app.rb
+++ app.rb
@@ -9,27 +9,38 @@ get '/user_name' do
 end
 
 post '/user_name' do
-	name_provided = params[:input_name]
+	name_provided = params[:name_provided]
     redirect '/age?name_provided=' + name_provided
 end
 
 get '/age' do
-	name_provided = params[:input_name]
-	puts "name_provided in get age #{name_provided}"
-	erb :age
+	name_provided = params[:name_provided]
+	# puts "name_provided in get age #{name_provided}"
+	erb :age, :locals => {:name_provided => name_provided}
 end
 
 post '/age' do
-	age_provided = params[:input_age]
-	redirect '/fave_nums?user_age' + user_age
+	age_provided = params[:age_provided]
+	name_provided = params[:name_provided]
+	# puts "name_provided in get age #{name_provided} and age_provided is #{age_provided}"
+	redirect '/fave_nums?age_provided=' + age_provided + '&name_provided=' + name_provided
 
 end
 
-# get 'fave_nums' do
-# 	erb :fave_nums
-# end
+get '/fave_nums' do
+	age_provided = params[:age_provided]
+	name_provided = params[:name_provided]
+	puts "age_provided in get fav nums #{age_provided}"
+	puts "name_provided in get fav nums #{name_provided}"
+	erb :fave_nums, :locals => {:age_provided => age_provided, :name_provided => name_provided}
+end
 
-# post '/fave_nums' do
-#   	nums_provided = params[:input_nums]
-# 	redirect '/nums?user_fave_nums' + user_fave_nums
-# end
+post '/fave_nums' do
+	age_provided = params[:age_provided]
+	name_provided = params[:name_provided]
+  	nums_provided = params[:input_nums]
+ #  	puts "age_provided in get nums #{age_provided}"
+	# puts "name_provided in get age #{name_provided}"
+	# puts "nums_provided in get age #{nums_provided}"
+
+end
