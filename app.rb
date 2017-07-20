2b9e5226b7b8f647dddb520a3e298a9e3f5257d8
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-07-20 12:01:18 -0400

    sorta better with javascript in it

--- app.rb
+++ app.rb
@@ -10,35 +10,41 @@ end
 
 post '/user_name' do
 	name_provided = params[:name_provided]
-    redirect '/age?name_provided=' + name_provided
+	logindate = params[:logindate]
+	puts "logindate at user_name is #{logindate}"
+    redirect '/age?name_provided=' + name_provided + '&logindate=' + logindate
 end
 
 get '/age' do
 	name_provided = params[:name_provided]
-	# puts "name_provided in get age #{name_provided}"
-	erb :age, :locals => {:name_provided => name_provided}
+	logindate = params[:logindate]
+	puts "logindate provided in get age function is #{logindate}"
+	erb :age, :locals => {:name_provided => name_provided, :logindate => logindate}
 end
 
 post '/age' do
 	age_provided = params[:age_provided]
 	name_provided = params[:name_provided]
-	# puts "name_provided in get age #{name_provided} and age_provided is #{age_provided}"
-	redirect '/fave_nums?age_provided=' + age_provided + '&name_provided=' + name_provided
+	logindate = params[:logindate]
+	puts "logindate provided in post age function is #{logindate}"
+	redirect '/fave_nums?age_provided=' + age_provided + '&name_provided=' + name_provided + '&logindate=' + logindate
 
 end
 
 get '/fave_nums' do
 	age_provided = params[:age_provided]
 	name_provided = params[:name_provided]
+	logindate = params[:logindate]
 	puts "age_provided in get fav nums #{age_provided}"
 	puts "name_provided in get fav nums #{name_provided}"
-	erb :fave_nums, :locals => {:age_provided => age_provided, :name_provided => name_provided}
+	erb :fave_nums, :locals => {:age_provided => age_provided, :name_provided => name_provided, :logindate => logindate}
 end
 
 post '/fave_nums' do
 	age_provided = params[:age_provided]
 	name_provided = params[:name_provided]
   	nums_provided = params[:input_nums]
+  	logindate = params [:logindate]
  #  	puts "age_provided in get nums #{age_provided}"
 	# puts "name_provided in get age #{name_provided}"
 	# puts "nums_provided in get age #{nums_provided}"
